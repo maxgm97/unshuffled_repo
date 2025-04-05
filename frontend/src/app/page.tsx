33f6f495572c830807d591f2e3feb79386c39ed2
Max Miller <maxgmiller87@gmail.com>
2025-04-04 23:01:20 -0400

    little bigger?

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -100,7 +100,7 @@ export default function ShufflePage() {
                     key={index}
                     src={cardNameToFilename(card)}
                     alt={card}
-                    style={{ width: '68px', height: 'auto', margin: '5px' }}
+                    style={{ width: '72px', height: 'auto', margin: '5px' }}
                 />
             ))}
             </div>
