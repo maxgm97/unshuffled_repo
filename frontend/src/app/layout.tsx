c6c78ee7e1e20164685fa9cde10a6ceb9433d149
Max Miller <maxgmiller87@gmail.com>
2025-04-05 16:18:39 -0400

    fix head?

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import './styles.css';
-import Head from "next/head";
+//import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
