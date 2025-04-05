428e38d9e2d9afb7a30e247893de2a38db7f7872
Max Miller <maxgmiller87@gmail.com>
2025-04-05 16:23:12 -0400

    add favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/images/favicon.ico" type="image/x-icon"/>
         <title>Shuffle please</title>
       </head>
       <body
