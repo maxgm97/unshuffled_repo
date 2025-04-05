980a65ee090260f9c95481cf00c3238e2eb16b8d
Max Miller <maxgmiller87@gmail.com>
2025-04-04 22:28:27 -0400

    make cards bigger

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -100,7 +100,7 @@ export default function ShufflePage() {
                         key={index}
                         src={cardNameToFilename(card)}
                         alt={card}
-                        style={{ width: '80px', height: 'auto', margin: '5px' }}
+                        style={{ width: '95px', height: 'auto', margin: '5px' }}
                     />
                 ))}
             </div>
