9bfe9be85886a06d994c3c5d984c39f242285977
Max Miller <maxgmiller87@gmail.com>
2025-04-04 20:30:19 -0400

    comment out axios

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 
 const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
